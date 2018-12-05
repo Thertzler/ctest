b00ab0a20c7edf2bcf3e0635ab127959b2da76d2
HertzlerT <timo.hertzler@gmail.com>
2018-12-05 23:02:04 +0100

    24 hour support

--- js/cscript.js
+++ js/cscript.js
@@ -1,9 +1,23 @@
 var handH = document.getElementById("zh");
 var handM = document.getElementById("zm");
 var handS = document.getElementById("zs");
-var pfix = "translateX(-50%)"
+var change = document.getElementById("clockChange")
+
 var mainClock = document.getElementById("cbody");
-var numCount=5;
+var pfix = "translateX(-50%)"
+var numDiv=1;
+
+change.addEventListener("click",function(){
+delFace();
+if(numDiv == 1){
+ numDiv = 2;
+ change.setAttribute("value","12 Hours")
+}else{
+change.setAttribute("value","24 Hours")
+numDiv = 1;
+}
+drawFace();
+})
 
 function genLine(_class,_rot){
 var newLine= document.createElement("div");
@@ -11,25 +25,32 @@ newLine.classList.add("nline",_class)
 newLine.style.transform = pfix+" rotate("+ setRot(60,0,_rot) +"deg)";
 if(_class!=="mini"){
 var numero=document.createElement("p");
-numero.textContent= _rot/numCount;
+numero.textContent= _rot/(5/numDiv);
 numero.style.transform = pfix+" rotate("+ setRot(60,0,_rot)*-1 +"deg)";
 newLine.appendChild(numero)
 }
 mainClock.appendChild(newLine)
 }
-
-for(var i=1; i<61; ++i)
+function drawFace(){
+for(var i=0.5; i<60.5; i += 0.5)
 {
-    if(i%numCount == 0){
-        if(i%(numCount*3) == 0)
+    if(i%(5/numDiv) == 0){
+        if(i%((5/numDiv)*3) == 0)
             {genLine("quarter",i)}
         else
             {genLine("medium",i)}
     }
     else
     {genLine("mini",i)}
-}
+}}
+drawFace();
 
+function delFace(){
+    lineList = document.getElementsByClassName("nline");
+    for(var i=lineList.length; i > 0; --i){
+    lineList[i-1].parentNode.removeChild(lineList[i-1])
+    }
+    }
 
 
 function setRot(_fraction, _modus, _manum){
@@ -44,7 +65,7 @@ function setRot(_fraction, _modus, _manum){
         addition = sTime.getSeconds()/60;
     }
     else if (_modus == 2){
-        rotation = (sTime.getHours() > 12) ? sTime.getHours() - 12 : sTime.getHours;
+        rotation = sTime.getHours();
         addition = sTime.getMinutes()/60;
     }
 
@@ -54,7 +75,7 @@ function setRot(_fraction, _modus, _manum){
 function clockUpdate(){
     handS.style.transform=pfix+" rotate("+ setRot(60,0) +"deg)";
     handM.style.transform=pfix+" rotate("+ setRot(60,1) +"deg)";
-    handH.style.transform=pfix+" rotate("+ setRot(12,2) +"deg)";
+    handH.style.transform=pfix+" rotate("+ setRot(12*numDiv,2) +"deg)";
 }
 
 setInterval(clockUpdate,1000)
