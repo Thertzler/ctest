099c48ecda052fcdde4bc7eee4de89bac52bdb7f
Timo Hertzler <timo.hertzler@netlution.de>
2018-12-05 14:02:26 +0100

    JS Optimierung

--- js/cscript.js
+++ js/cscript.js
@@ -13,18 +13,18 @@ function setRot(_fraction, _modus){
 
     if(_modus == 1){addition= sTime.getSeconds()/60}
     else if(_modus==2){addition= sTime.getMinutes()/60}
+
     return(( (rotation+addition) / _fraction)*360)-180;
 }
 
 function clockUpdate(){
-var valS = setRot(60,0);
-var valM = setRot(60,1);
-var valH = setRot(12,2);
+    var valS = setRot(60,0);
+    var valM = setRot(60,1);
+    var valH = setRot(12,2);
 
-handS.style.transform="rotate("+ valS+"deg)";
-handM.style.transform="rotate("+ valM+"deg)";
-handH.style.transform="rotate("+ valH+"deg)";
-
+    handS.style.transform="rotate("+ valS+"deg)";
+    handM.style.transform="rotate("+ valM+"deg)";
+    handH.style.transform="rotate("+ valH+"deg)";
 }
 
 setInterval(clockUpdate,1000)
