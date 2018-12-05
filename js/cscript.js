4b2b56438d7fc0987714f69d355ab7acf6dfb2da
HertzlerT <timo.hertzler@gmail.com>
2018-12-05 22:08:50 +0100

    Stripes added

--- js/cscript.js
+++ js/cscript.js
@@ -1,14 +1,37 @@
 var handH = document.getElementById("zh");
 var handM = document.getElementById("zm");
 var handS = document.getElementById("zs");
-
 var pfix = "translateX(-50%)"
+var mainClock = document.getElementById("cbody");
 
-function setRot(_fraction, _modus){
+function genLine(_class,_rot,_num){
+var newLine= document.createElement("div");
+newLine.classList.add("nline",_class)
+newLine.style.transform = pfix+" rotate("+ setRot(60,0,_rot) +"deg)";
+if(_num){
+var numero=document.createElement("p");
+numero.textContent= _rot/5;
+newLine.appendChild(numero)
+}
+mainClock.appendChild(newLine)
+}
+
+for(var i=1; i<61; ++i)
+{
+
+    genLine("nline",i)
+
+
+}
+
+
+
+function setRot(_fraction, _modus, _manum){
     if(_fraction === 0){return 0}
     var sTime = new Date;
     var addition = 0;
-    var rotation = sTime.getSeconds();
+    var rotation = _manum? _manum: sTime.getSeconds();
+    var reverse = _manum? 0 : 180;
 
     if(_modus == 1){
         rotation = sTime.getMinutes();
@@ -19,7 +42,7 @@ function setRot(_fraction, _modus){
         addition = sTime.getMinutes()/60;
     }
 
-    return (((rotation+addition) / _fraction)*360)-180;
+    return (((rotation+addition) / _fraction)*360)-reverse;
 }
 
 function clockUpdate(){
