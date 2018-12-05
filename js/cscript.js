d26cdcac41b0c48b3184f5fafddb93798c7a0884
Timo Hertzler <timo.hertzler@netlution.de>
2018-12-05 13:19:08 +0100

    Zentrierter Style

--- js/cscript.js
+++ js/cscript.js
@@ -2,21 +2,29 @@ var handH = document.getElementById("zh");
 var handM = document.getElementById("zm");
 var handS = document.getElementById("zs");
 
-function setRot(_fraction, _rotation){
-if(_fraction === 0){return 0}
-return((_rotation/_fraction)*360)-180;
+function setRot(_fraction, _modus){
+    if(_fraction === 0){return 0}
+    var sTime = new Date;
+    var addition = 0;
+    var rotation = sTime.getSeconds();
+
+    if(_modus == 1){rotation = sTime.getMinutes();}
+    else if(_modus == 2){rotation = (sTime.getHours() > 12) ? sTime.getHours() - 12 : sTime.getHours()}
+
+    if(_modus == 1){addition= sTime.getSeconds()/60}
+    else if(_modus==2){addition= sTime.getMinutes()/60}
+    return(( (rotation+addition) / _fraction)*360)-180;
 }
 
 function clockUpdate(){
-var sTime = new Date;
-var valS = setRot(60,sTime.getSeconds());
-var valM = setRot(60,sTime.getMinutes());
-var valH = setRot(12,sTime.getHours());
+var valS = setRot(60,0);
+var valM = setRot(60,1);
+var valH = setRot(12,2);
+
 handS.style.transform="rotate("+ valS+"deg)";
 handM.style.transform="rotate("+ valM+"deg)";
 handH.style.transform="rotate("+ valH+"deg)";
 
-console.log(valH)
 }
 
 setInterval(clockUpdate,1000)
